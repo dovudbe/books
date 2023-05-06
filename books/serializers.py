e9ce723684ef02a71312aca75256c77f49108e47
dovudbe <131879667+dovudbe@users.noreply.github.com>
2023-05-06 16:18:44 +0500

    project

---
+++ books/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import Book
+from rest_framework.exceptions import ValidationError
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('id','title','subtitle','content','author','isbn','price',)
+
+    def validate(self, data):
+        title = data.get('title', None)
+        author = data.get('author', None)
+        if not title.isalpha():
+            raise ValidationError(
+                {
+                    "status": False,
+                    "message": "kitobni sarlavhasi harflardan tashkil topgan bo'lishi kerak",
+                }
+            )
+
+        if Book.objects.filter(title=title, author=author,).exists():
+            raise ValidationError(
+                {
+                    "status": False,
+                    "message": "kitob sarlavhasi va muallifi bir xil  bo'lgan kitobni yuklay olmaysiz",
+                }
+            )
+
+        return data
+
+def validate_price(self, price):
+    if price < 0 or price > 9999999:
+        raise ValidationError(
+            {
+                "status": False,
+                "message": "narx noto'g'ri kiritilgan",
+            }
+        )
